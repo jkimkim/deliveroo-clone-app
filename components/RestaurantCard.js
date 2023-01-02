237d1a181626a5dc4bf4f42560d199d9d3597129
JOHN <65081425+jkimkim@users.noreply.github.com>
2023-01-02 12:43:01 +0300

    modified:   components/FeaturedRow.js 	new file:   components/RestaurantCard.js 	modified:   tailwind.config.js

---
+++ components/RestaurantCard.js
@@ -0,0 +1,29 @@
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+import React from 'react'
+
+const RestaurantCard = ({
+    id,
+    imgUrl,
+    title,
+    rating,
+    genre,
+    address,
+    short_desc,
+    dishes,
+    long,
+    lat,
+}) => {
+  return (
+    <TouchableOpacity>
+          <Image
+              source={{ uri: imgUrl }}
+              className="h-36 w-64 rounded-sm"
+          />
+          <View className="px-3 pb-4">
+                <Text className="text-bold text-lg pt-2">{title}</Text>
+          </View>
+    </TouchableOpacity>
+  )
+}
+
+export default RestaurantCard
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.js", "./screens/HomeScreen.js", "./components/Categories.js", "./components/CategoriesCard.js", "./components/FeaturedRow.js"],
+  content: [
+    "./App.js",
+    "./screens/HomeScreen.js",
+    "./components/Categories.js",
+    "./components/CategoriesCard.js",
+    "./components/FeaturedRow.js",
+    "./components/RestaurantCard.js",
+  ],
   theme: {
     extend: {},
   },
