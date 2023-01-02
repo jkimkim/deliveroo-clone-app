ca939790a66afbac13b2728e90aa95c3a7d03903
JOHN <65081425+jkimkim@users.noreply.github.com>
2023-01-02 10:01:10 +0300

    modified:   components/CategoriesCard.js

--- components/CategoriesCard.js
+++ components/CategoriesCard.js
@@ -8,7 +8,9 @@ const CategoriesCard = ({imgUrl,title}) => {
               source={{ uri: imgUrl , }}
               style={{ width: 120, height: 120, resizeMode: 'cover', borderRadius: 10 }}
           />
-      <Text>{title}</Text>
+          <Text className="absolute bottom-1 left-1 text-white font-bold">
+              {title}
+          </Text>
     </TouchableOpacity>
   )
 }
