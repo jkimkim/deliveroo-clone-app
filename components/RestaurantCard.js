a0f36799b33d18b39547b1f191592fee53cbc1be
JOHN <65081425+jkimkim@users.noreply.github.com>
2023-01-26 09:27:00 +0300

    modified:   components/RestaurantCard.js

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -1,27 +1,34 @@
 import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
+import { StarIcon } from 'react-native-heroicons/outline'
 
 const RestaurantCard = ({
-    id,
-    imgUrl,
-    title,
-    rating,
-    genre,
-    address,
-    short_desc,
-    dishes,
-    long,
-    lat,
+  id,
+  imgUrl,
+  title,
+  rating,
+  genre,
+  address,
+  short_desc,
+  dishes,
+  long,
+  lat,
 }) => {
   return (
     <TouchableOpacity>
-          <Image
-              source={{ uri: imgUrl }}
-              className="h-36 w-64 rounded-sm"
-          />
-          <View className="px-3 pb-4">
-                <Text className="text-bold text-lg pt-2">{title}</Text>
-          </View>
+      <Image
+        source={{ uri: imgUrl }}
+        className="h-36 w-64 rounded-sm"
+      />
+      <View className="px-3 pb-4">
+        <Text className="text-bold text-lg pt-2">
+          {title}
+        </Text>
+        <View className="flex-row items-center space-x-1">
+          <StarIcon color="green" opacity={0.5} size={22} />
+          <Text>{ rating}</Text>
+        </View>
+      </View>
     </TouchableOpacity>
   )
 }
